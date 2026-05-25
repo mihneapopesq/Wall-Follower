307043160d56af72b000436d94ed0ad9efadca3d
mihneapopesq <popescumihneagabriel@gmail.com>
2026-05-25 03:14:17 +0300

    update logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,42 @@
+#ifndef F_CPU
 #define F_CPU 16000000UL
+#endif
 #include <avr/io.h>
 #include <util/delay.h>
 
-// Setari PID si navigare
-#define DIST_REF 10      // Distanta dorita fata de perete (cm)
-#define BASE_SPEED 150   // Viteza de baza (0-255)
-#define Kp 3.0           // Constanta proportionala
-#define Kd 1.5           // Constanta derivativa
+#define BASE_SPEED 200
+#define WALL_THRESH 15
+#define FRONT_THRESH 15
 
-int last_error = 0;
+float Kp = 5.0, Kd = 35.0; 
+float oldError = 0;
+int offset = 5;
+
+float oldL = 0, oldR = 0, oldF = 0;
+bool first_turn = false;
+bool rightWallFollow = false, leftWallFollow = false;
 
 void init_hardware() {
-    // Pini Directie Motoare (PD4, PD5, PD6, PD7) ca OUTPUT
     DDRD |= (1 << PD4) | (1 << PD5) | (1 << PD6) | (1 << PD7);
-    
-    // Pini PWM: ENA(PB2/D10), ENB(PB3/D11) ca OUTPUT
     DDRB |= (1 << PB2) | (1 << PB3);
-    
-    // Pini Senzori (Trig=OUT, Echo=IN)
-    DDRD |= (1 << PD2); DDRD &= ~(1 << PD3); // Stanga: Trig D2, Echo D3
-    DDRB |= (1 << PB0); DDRB &= ~(1 << PB1); // Mijloc: Trig D8, Echo D9
-    DDRB |= (1 << PB4); DDRB &= ~(1 << PB5); // Dreapta: Trig D12, Echo D13
+    DDRD |= (1 << PD2); DDRD &= ~(1 << PD3); // Stânga
+    DDRB |= (1 << PB0); DDRB &= ~(1 << PB1); // Mijloc
+    DDRB |= (1 << PB4); DDRB &= ~(1 << PB5); // Dreapta
 
-    // Configurare Timer1 (PWM 8-bit pe PB2 - ENA)
     TCCR1A |= (1 << COM1B1) | (1 << WGM10);
-    TCCR1B |= (1 << WGM12) | (1 << CS11); // Prescaler 8
+    TCCR1B |= (1 << WGM12) | (1 << CS11); 
     
-    // Configurare Timer2 (Fast PWM pe PB3 - ENB)
     TCCR2A |= (1 << COM2A1) | (1 << WGM21) | (1 << WGM20);
-    TCCR2B |= (1 << CS21); // Prescaler 8
+    TCCR2B |= (1 << CS21); 
 }
 
 void set_motors(int speedL, int speedR) {
-    // Directie Stanga (IN1=PD4, IN2=PD5)
     if (speedL >= 0) { PORTD |= (1 << PD4); PORTD &= ~(1 << PD5); }
     else { PORTD &= ~(1 << PD4); PORTD |= (1 << PD5); speedL = -speedL; }
     
-    // Directie Dreapta (IN3=PD6, IN4=PD7)
     if (speedR >= 0) { PORTD |= (1 << PD6); PORTD &= ~(1 << PD7); }
     else { PORTD &= ~(1 << PD6); PORTD |= (1 << PD7); speedR = -speedR; }
 
-    // Aplicare PWM cu limitare 0-255
     OCR1B = (speedL > 255) ? 255 : speedL;
     OCR2A = (speedR > 255) ? 255 : speedR;
 }
@@ -56,53 +52,66 @@ uint16_t read_distance(volatile uint8_t *port, volatile uint8_t *pin_reg, uint8_
     uint32_t duration = 0;
     while (*pin_reg & (1 << echo)) { duration++; _delay_us(1); if (duration > 20000) return 999; }
     
-    return duration * 0.01715; // Conversie in cm
+    return duration * 0.01715; 
 }
 
 int main() {
     init_hardware();
-    //DIS = 20 pentru spatiu de franare, dar folosimcommit forward
-    int DIS = 20; 
 
     while (1) {
-        uint16_t dFront = read_distance(&PORTB, &PINB, PB0, PB1);
-        
-        if (dFront < DIS) {
-            // S-a oprit la zid.
-            set_motors(0, 0);
-            _delay_ms(200); // Pauza scurta sa se stabilizeze senzorii
-            
-            uint16_t dRight = read_distance(&PORTB, &PINB, PB4, PB5);
-            uint16_t dLeft  = read_distance(&PORTD, &PIND, PD2, PD3);
+        float f = read_distance(&PORTB, &PINB, PB0, PB1);
+        float r = read_distance(&PORTB, &PINB, PB4, PB5);
+        float l = read_distance(&PORTD, &PIND, PD2, PD3);
 
-            if (dRight > dLeft) {
-                // Dreapta e mai libera -> Intoarcere ~90 grade dreapta
-                set_motors(255, -255); 
-                _delay_ms(350); 
-            } else {
-                // Stanga e mai libera -> Intoarcere ~90 grade stanga
-                set_motors(-255, 255); 
-                _delay_ms(350); 
-            }
+        if (f == 999) f = 50; 
+        if (r == 999) r = 50; 
+        if (l == 999) l = 50;
 
-            // Faza obligatorie de mers inainte "commit"
-            // Aceasta forteaza robotul sa iasa din zona de conflict
-            // Mergem inainte un pic (de exemplu 200ms) 
-            set_motors(255, 255); 
-            _delay_ms(200);
-            
-            // last_error = 0;        // PID logic reset is important
-            // set_motors(0, 0); // Faza de cooldown - oprim motoarele
-            // _delay_ms(100);
+        float front = (f + oldF) / 2.0; oldF = front;
+        float right = (r + oldR) / 2.0; oldR = right;
+        float left = (l + oldL) / 2.0;  oldL = left;
 
-            // last_error = 0;        // Reset PID after maneuver
-            continue;              // Sarim peste restul logicii in acest ciclu
-        } 
-        else {
-            // Nu are obstacol in fata -> inainte
-            set_motors(255, 255);
+        bool frontwall = (front < FRONT_THRESH);
+        bool rightwall = (right < WALL_THRESH);
+        bool leftwall = (left < WALL_THRESH);
+
+        if (leftwall && !rightwall && frontwall && !first_turn) {
+            first_turn = true;
+            rightWallFollow = true;
         }
-        
+        if (!leftwall && rightwall && frontwall && !first_turn) {
+            first_turn = true;
+            leftWallFollow = true;
+        }
+
+        if (frontwall) {
+            if (right > left) {
+                set_motors(255, -255);
+            } else {
+                set_motors(-255, 255);
+            }
+            _delay_ms(350);
+            oldError = 0; 
+        } else {
+            float error = left - right;
+            if (rightWallFollow) error -= offset;
+            else if (leftWallFollow) error += offset;
+
+            float deriv = error - oldError;
+            oldError = error;
+            
+            int totalError = (Kp * error) + (Kd * deriv);
+            int RMS = BASE_SPEED + totalError;
+            int LMS = BASE_SPEED - totalError;
+
+            if (RMS > 255) { RMS = 255; }
+            if (RMS < 0)   { RMS = 0; }
+            if (LMS > 255) { LMS = 255; }
+            if (LMS < 0)   { LMS = 0; }
+
+            set_motors(LMS, RMS);
+        }
+
         _delay_ms(50);
     }
 }
